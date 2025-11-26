90e75de5bf310d6e0ba2bbeecdef43509b258d37
mycodesgit <lowexian@gmail.com>
2025-11-26 14:39:40 +0800

    Update

--- resources/views/modal/itemAddmodal.blade.php
+++ resources/views/modal/itemAddmodal.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="addItemModal" tabindex="-1" role="dialog" aria-labelledby="addItemModalLabel" aria-hidden="true">
+<div class="modal fade" id="addItemModal" role="dialog" aria-labelledby="addItemModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-md modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
